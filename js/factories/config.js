0566b14423a33543a1db95603ba06830cfc7c1da
root <root@vps609903.ovh.net>
2019-07-25 10:12:21 +0200

    Fix images in answer list. TODO: fix syntax src ng-src

--- js/factories/config.js
+++ js/factories/config.js
@@ -1,1 +1,14 @@
-const quizID = '15';
+var quizID = '15';
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+if(getUrlVars()["quizID"]){
+  quizID = getUrlVars()["quizID"];
+}
+
